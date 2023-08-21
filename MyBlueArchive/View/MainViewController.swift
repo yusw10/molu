3f7ebce2fc6619311c7ee17092fcf114f49354d8
hansukYU <yusw10@naver.com>
2023-08-21 18:29:15 +0900

    feat: Network(URLSession) write
    - 네트워크 코드 작성중

--- MyBlueArchive/View/MainViewController.swift
+++ MyBlueArchive/View/MainViewController.swift
@@ -12,6 +12,18 @@ final class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        let targetURL = "https://api-blue-archive.vercel.app/api/characters"
+        guard let url = URL(string: targetURL) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        let urlSession = URLSession(configuration: .default)
+        urlSession.dataTask(with: request) { data, response, error in
+            
+        }
+    
     }
 }
 
